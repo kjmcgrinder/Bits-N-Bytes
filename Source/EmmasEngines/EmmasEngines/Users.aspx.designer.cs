2c0b54904e04f847fc40d26fd18cafb604569249
yorguin <helpdesk@niagaracollege.ca>
2018-11-26 08:21:41 -0500

    Revert "fix exception for users"
    This reverts commit 673737ac5107a6064810da1269d691500745d1d1.

--- Source/EmmasEngines/EmmasEngines/Users.aspx.designer.cs
+++ Source/EmmasEngines/EmmasEngines/Users.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace EmmasEngines {
     public partial class Users {
         
         /// <summary>
-        /// Form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
-        
-        /// <summary>
         /// ddlDeparment control.
         /// </summary>
         /// <remarks>
