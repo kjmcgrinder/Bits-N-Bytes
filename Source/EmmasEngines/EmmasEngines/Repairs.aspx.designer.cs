75436c69ff2c2ca023fa84f7e2b8ec92cd9d8575
kjmcgrinder <kjmcgrinder@gmail.com>
2018-11-27 09:37:21 -0500

    created an in-progress dataset for the repair page

--- Source/EmmasEngines/EmmasEngines/Repairs.aspx.designer.cs
+++ Source/EmmasEngines/EmmasEngines/Repairs.aspx.designer.cs
@@ -31,13 +31,13 @@ namespace EmmasEngines {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
         
         /// <summary>
-        /// GridView1 control.
+        /// repairGridview control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView repairGridview;
         
         /// <summary>
         /// RepairDataSource control.
