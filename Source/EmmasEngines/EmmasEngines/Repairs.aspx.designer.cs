dcb10969b7c2a7f9adbfd3fdd4cef44083351b82
kjmcgrinder <kjmcgrinder@gmail.com>
2018-11-27 09:16:55 -0500

    Modified repair gridview

--- Source/EmmasEngines/EmmasEngines/Repairs.aspx.designer.cs
+++ Source/EmmasEngines/EmmasEngines/Repairs.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace EmmasEngines {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// RepairDataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource RepairDataSource;
     }
 }
