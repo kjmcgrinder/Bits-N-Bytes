71f9c44d1dd8e4caf744dc1b774234dd77d0fd9b
Kevin McGrinder <kjmcgrinder@gmail.com>
2018-12-11 12:03:23 -0500

    made welcome message to welcome firstname not username

--- Source/EmmasEngines/EmmasEngines/Default.aspx.cs
+++ Source/EmmasEngines/EmmasEngines/Default.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using EmmasEnginesLibrary.EmployeeDataSetTableAdapters;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +15,28 @@ namespace EmmasEngines
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //get the first name of the user for the welcome message
+            var roleStore = new RoleStore<IdentityRole>();
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+            positionTableAdapter daPositions = new positionTableAdapter();
+            employeeTableAdapter daEmployees = new employeeTableAdapter();
+            DataTable posTable = daPositions.GetData();
+            EmmasEnginesLibrary.EmployeeDataSet dsEmployee = new EmmasEnginesLibrary.EmployeeDataSet();
+            daEmployees.Fill(dsEmployee.employee);
+            foreach (DataRow r in posTable.Rows)
+            {
+                bool exists = roleManager.RoleExists(r["posName"].ToString());
+                if (!exists)
+                    roleManager.Create(new IdentityRole(r["posName"].ToString()));
+            }
+            string welcomeName;
+            foreach (DataRow r in dsEmployee.employee.Rows)
+            {
+                welcomeName = r["empFirst"].ToString();
+                Label1.Text = welcomeName.ToString();
+            }
         }
     }
 }
